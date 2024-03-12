71ce3331e1b2b4e28fd137a9c403d27f64169295
VaishnavVerma <vaishnavverma0@gmail.com>
2024-03-12 13:05:25 +0530

    feat: Add new function to get the title

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ from date_generator import generate_apod_urls
 def process_response(response):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')  # Specify parser explicitly
-        paragraph_tag = soup.find('p')
-        desired_text = paragraph_tag.get_text(strip=True).split('.')[0]
+        
+        # Find the title tag and get its content
+        title_tag = soup.find('title')
 
-        image_tag = soup.find('img')
-        if image_tag:
-            image_url = urljoin(response.url, image_tag['src'])
-            date = response.url[-11:-5]
-            file_id = date
+        if title_tag:
+            title_content = title_tag.get_text(strip=True)
 
-            data = {
-                'id': file_id,
-                'Archivepix': desired_text,
-                'image_url': image_url,
-                'web_page_url': response.url
-            }
+            image_tag = soup.find('img')
+            if image_tag:
+                image_url = urljoin(response.url, image_tag['src'])
+                date = response.url[-11:-5]
+                file_id = date
 
-            json_data = json.dumps(data, indent=4)
+                data = {
+                    'id': file_id,
+                    'title': title_content,
+                    'image_url': image_url,
+                    'web_page_url': response.url
+                }
 
-            with open('image_data.json', 'a') as json_file:
-                json_file.write(json_data + '\n')
-            print("Image ID, URL, and webpage URL saved to 'image_data.json'.")
+                json_data = json.dumps(data, indent=4)
+
+                with open('image_data.json', 'a') as json_file:
+                    json_file.write(json_data + '\n')
+                print("Image ID, URL, and webpage URL saved to 'image_data.json'.")
+            else:
+                print("No image found on the page.")
         else:
-            print("No image found on the page.")
+            print("No title found on the page.")
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
